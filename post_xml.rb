7ad9df47a8db541a349db1e41795cacbd11c4bb0
Skyrda Dima <skyrda_dima@yahoo.com>
2015-09-22 16:09:10 +0300

    add status request

--- post_xml.rb
+++ post_xml.rb
@@ -79,5 +79,74 @@ module BSG
 			doc = Nokogiri::XML(response.body)
 			print doc.xpath('//@id_sms').map(&:value)
 		end
+
+		def get_status(smscid)
+			
+			smscid = smscid.map(&:to_s).join(';') if smscid.is_a? Array
+			      
+			payload = Nokogiri::XML::Builder.new do |xml|
+			    xml.request {
+					xml.get_state {
+					        xml.id_sms smscid
+					}
+					xml.security {
+						xml.login('value' => @login)
+						xml.password('value' => @password)
+					}
+				}
+			end
+
+			uri = URI.parse('http://app.bsg.hk/xml/stat')
+			request = Net::HTTP::Post.new uri.path
+			request.body = payload.to_xml
+			request.content_type = 'text/xml'
+			response = Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
+
+			doc = Nokogiri::XML(response.body)
+			a = doc.xpath('//response/state').map do |status|
+				{ status.xpath('@id_sms').text => status.xpath('//state').text.downcase.to_sym }
+			end
+			print a
+
+    	end
+
+		def get_statuses(smscid)
+			
+			smscid = smscid.map(&:to_s) if smscid.is_a? Array
+	        hash = Hash.new
+			smscid.each_with_index {|item, index|
+				hash[item] = index
+			}
+			state = hash
+			      
+			payload = Nokogiri::XML::Builder.new do |xml|
+			    xml.request {
+					xml.get_state {
+						state.each do |key, value|
+					        xml.id_sms key
+					    end
+					}
+					xml.security {
+						xml.login('value' => @login)
+						xml.password('value' => @password)
+					}
+				}
+			end
+
+			uri = URI.parse('http://app.bsg.hk/xml/stat')
+			request = Net::HTTP::Post.new uri.path
+			request.body = payload.to_xml
+			request.content_type = 'text/xml'
+			response = Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
+
+			doc = Nokogiri::XML(response.body)
+			a = doc.xpath('//response/state').map do |status|
+				{ status.xpath('@id_sms').text => status.xpath('//state').text.downcase.to_sym }
+			end
+			print a
+
+    	end
+
+
 	end
 end
